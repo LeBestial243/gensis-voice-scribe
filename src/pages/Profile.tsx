879c10911bc89a454ec6b6933c74e3a543c2897b
LeBestial243 <felly.lunkeba@gmail.com>
2025-05-03 22:17:32 +0200

    Delete src/pages/Profile.tsx

--- src/pages/Profile.tsx
+++
@@ -1,9 +0,0 @@
-
-import { FolderDisplay } from "@/components/folder/FolderDisplay";
-
-export function Profile() {
-  return <FolderDisplay profileId="default" />;
-}
-
-// Re-export FolderDisplay for backward compatibility
-export { FolderDisplay };
